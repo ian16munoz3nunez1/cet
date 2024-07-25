77958d4f959fed523f1c6f92538223000306cc3d
ian16munoz3nunez1 <ianemn1603@gmail.com>
2024-07-25 12:41:10 -0600

    Cambios en main.py y setup.bat
    - Se agregó la hoja de estilos en el archivo main.py
    - Se agregó la ruta absoluta del archivo reboot.txt
    - Se agregó el parametro /f a setup.bat

--- main.py
+++ main.py
@@ -2,17 +2,38 @@
 
 import sys
 import os
+import getpass
 from PyQt5.QtWidgets import QApplication
 from start import Start
 from running import Running
 
 app = QApplication(sys.argv)
-with open("main.css", 'r') as file:
-    style = file.read()
-file.close()
+style = """
+QMainWindow {
+    background-color: rgb(100, 100, 100);
+}
+
+QLabel {
+    color: rgb(255, 255, 255);
+    font: 12pt 'monospace';
+}
+
+QSpinBox {
+    color: rgb(255, 255, 255);
+    font: 12pt 'monospace';
+    background-color: rgb(100, 100, 100);
+}
+
+QPushButton {
+    color: rgb(255, 255, 255);
+    font: 12pt 'monospace';
+}
+"""
 app.setStyleSheet(style)
 
+username = getpass.getuser()
 filename = 'reboot.txt'
+# filename = f'C:\\Users\\{username}\\Desktop\\reboot.txt'
 
 if not os.path.exists(filename):
     start = Start()
--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device" /v DevicePasswordLessBuildVersion /t REG_DWORD /d 0 /f
-reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v python /t REG_SZ /d "C:\Users\%USERNAME%\Desktop\main.exe"
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v python /t REG_SZ /d "C:\Users\%USERNAME%\Desktop\main.exe" /f
 
 netplwiz
 
